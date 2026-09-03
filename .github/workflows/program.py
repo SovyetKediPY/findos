d9e107458588cda3c5fdfc581f407326d07f9acb
SovyetKediPY <efehaydaroglu12@gmail.com>
2026-09-03 13:20:37 +0300

    Update program.py

--- .github/workflows/program.py
+++ .github/workflows/program.py
@@ -1,19 +1,1 @@
-print("Merhaba ! Bugün sana hangi işletim sistemini kullanman gerektiği hakkında bir test yapıcaz")
 
-gizlilik = input("Gizliliğe önem verir misin? (Evet veya Hayır): ").strip().lower()
-
-if gizlilik == "evet":
-    windows = False
-    macos = False
-    linux = True
-    print("Linux kullanmalısın")
-
-elif gizlilik == "hayır" or gizlilik == "hayir":
-    windows = True
-    macos = True
-    linux = False
-    oyun = input("Oyun oynar mısın? (Evet veya Hayır): ").strip().lower()
-    if oyun == "evet":
-        print("Windows kullanmalısın")
-    else:
-        print("Macos kullanmalısın ")
